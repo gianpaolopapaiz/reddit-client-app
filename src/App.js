3b072cc0ddd7690cb05981c7e75d85818d3501b3
gianpaolo papaiz <gipppppa@hotmail.com>
2021-03-17 11:20:03 -0300

    add seach bar feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import {NavBar} from './components/navBar/NavBar';
+import {Search} from './features/searchBar/Search';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Search/>
     </div>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import searchReduder from '../features/searchBar/searchSlice';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    search: searchReduder
   },
 });
