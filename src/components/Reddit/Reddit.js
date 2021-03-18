2a8c8d8fa0953f01d39871d9c9691d27f821a98a
gianpaolo papaiz <gipppppa@hotmail.com>
2021-03-18 10:13:31 -0300

    add popular reddirs complete

--- src/components/Reddit/Reddit.js
+++ src/components/Reddit/Reddit.js
@@ -5,10 +5,15 @@ export const Reddit = ({reddit}) => {
     return (
         <div className='reddit'>
             <h3>{reddit.data.title}</h3>
-            <img className='reddits-img' src={reddit.data.url_overridden_by_dest} alt=''/>
-            <p>{reddit.data.author}</p>
-            <p>{reddit.data.num_comments}</p>
-            <p>{reddit.data.permalink}</p>
+            <img className='reddit-img' src={reddit.data.url_overridden_by_dest} alt=''/>
+            <div className='reddit-info'>
+                <p className="reddit-author">Posted by <a className='reddit-author-link' href={`https://www.reddit.com/user/${reddit.data.author}/`} target='_blank' >{reddit.data.author}</a></p>
+                <div className='reddit-comments'>
+                    <p>{`Comments: ${reddit.data.num_comments}`}</p>
+                </div>
+                <a className='reddit-button' href={`https://www.reddit.com/${reddit.data.permalink}`} target='_blank'> View</a>
+            </div>
+            
         </div>    
     )
 }
